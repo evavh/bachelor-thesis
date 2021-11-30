02ea2af15e7ee873ce2882103b003d8896926863
Eva van Houten <user@server.fake>
2021-11-30 14:16:28 +0100

    Simplified test and added -e -1 test.

--- testing/run_and_compare.sh
+++ testing/run_and_compare.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 mkdir -p simulation/output
+
 python3 simulation/main.py -s 888 -n 20 -t 3 > simulation/output/stdout
 echo "Test 1 diff:"
 diff simulation/output/final_state.png testing/correct_outputs/test1_final_state.png
-diff simulation/output/radii.png testing/correct_outputs/test1_radii.png
-diff simulation/output/stdout testing/correct_outputs/test1_stdout
-#python3 simulation/main.py -s 888 -n 20 -t 3 -e -1
+
+python3 simulation/main.py -s 888 -n 20 -t 3 -e -1 > simulation/output/stdout
+echo "Test 2 diff:"
+diff simulation/output/final_state.png testing/correct_outputs/test2_final_state.png
