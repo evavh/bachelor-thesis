88a84cb53333c89b8febe68af651773cc26725b8
Eva van Houten <user@server.fake>
2021-11-30 14:24:41 +0100

    Added simulation script to make epaper script.

--- send_to_ereader/send.sh
+++ send_to_ereader/send.sh
@@ -2,4 +2,6 @@
 
 date=$(date +"%Y-%m-%d")
 
-pdflatex --output-directory=/tmp --jobname="main.py_${date}" send_to_ereader/main.py.tex && rmapi put /tmp/main.py_${date}.pdf Uni/Code
+pdflatex --output-directory=/tmp --jobname="simulation-main.py_${date}" send_to_ereader/simulation-main.py.tex && rmapi put /tmp/simulation-main.py_${date}.pdf Uni/Code
+
+pdflatex --output-directory=/tmp --jobname="analysis-main.py_${date}" send_to_ereader/analysis-main.py.tex && rmapi put /tmp/analysis-main.py_${date}.pdf Uni/Code
