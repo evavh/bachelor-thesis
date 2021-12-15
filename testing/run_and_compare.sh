0fc64abe0866b98b5bdb1226adf7e8bd82d3e757
Eva van Houten <user@server.fake>
2021-12-15 14:40:49 +0100

    Changed run test to use csv instead of plot for reliability.

--- testing/run_and_compare.sh
+++ testing/run_and_compare.sh
@@ -4,8 +4,8 @@ mkdir -p simulation/output
 
 python3 simulation/main.py -s 888 -n 20 -t 3 > simulation/output/stdout
 echo "Test 1 diff:"
-diff simulation/output/final_state.png testing/correct_outputs/test1_final_state.png
+diff simulation/output/final_state.csv testing/correct_outputs/test1_final_state.csv
 
 python3 simulation/main.py -s 888 -n 20 -t 3 -e -1 > simulation/output/stdout
 echo "Test 2 diff:"
-diff simulation/output/final_state.png testing/correct_outputs/test2_final_state.png
+diff simulation/output/final_state.csv testing/correct_outputs/test2_final_state.csv
