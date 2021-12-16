ec278c2451c1c64c545d5573708359bedcc0f4e4
Eva van Houten <user@server.fake>
2021-12-16 11:27:49 +0100

    Changed test to print stderr output after diffs.

--- testing/run_and_compare.sh
+++ testing/run_and_compare.sh
@@ -2,10 +2,13 @@
 
 mkdir -p simulation/output
 
-python3 simulation/main.py -s 888 -n 20 -t 3 > simulation/output/stdout
+error1=$(python3 simulation/main.py -s 888 -n 20 -t 3 2>&1 >simulation/output/stdout)
 echo "Test 1 diff:"
 diff simulation/output/final_state.csv testing/correct_outputs/test1_final_state.csv
 
-python3 simulation/main.py -s 888 -n 20 -t 3 -e -1 > simulation/output/stdout
+error2=$(python3 simulation/main.py -s 888 -n 20 -t 3 -e -1 2>&1 >simulation/output/stdout)
 echo "Test 2 diff:"
 diff simulation/output/final_state.csv testing/correct_outputs/test2_final_state.csv
+
+echo "Errors (test 1 only):"
+echo $error1
