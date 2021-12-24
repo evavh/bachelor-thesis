fac46bc93e09f47500b04c5b498fd39d2e3506cb
Eva van Houten <user@server.fake>
2021-12-24 12:27:33 +0100

    Changed test to use minimum Eb parameter.

--- testing/run_and_compare.sh
+++ testing/run_and_compare.sh
@@ -2,11 +2,11 @@
 
 mkdir -p simulation/output
 
-error1=$(python3 simulation/main.py -s 888 -n 20 -t 3 -b 0.1 2>&1 >simulation/output/stdout)
+error1=$(python3 simulation/main.py -s 888 -n 20 -t 3 2>&1 >simulation/output/stdout)
 echo "Test 1 diff:"
 diff simulation/output/final_state.csv testing/correct_outputs/test1_final_state.csv
 
-error2=$(python3 simulation/main.py -s 888 -n 20 -t 3 -b 0.1 -e -1 2>&1 >simulation/output/stdout)
+error2=$(python3 simulation/main.py -s 888 -n 20 -t 3 -e -1 2>&1 >simulation/output/stdout)
 echo "Test 2 diff:"
 diff simulation/output/final_state.csv testing/correct_outputs/test2_final_state.csv
 
