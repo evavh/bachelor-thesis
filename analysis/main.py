75b26d22b7c10d91e98ef910034f5ccdd82d4fb1
Eva van Houten <user@server.fake>
2022-01-11 14:00:06 +0100

    Added -i and -o arguments to analysis for input and output dirs.

--- analysis/main.py
+++ analysis/main.py
@@ -3,6 +3,7 @@ from amuse.units import nbody_system
 from matplotlib import pyplot
 import pickle
 import os
+import argparse
 
 
 def create_directory(directory_name):
@@ -10,44 +11,59 @@ def create_directory(directory_name):
         os.makedirs(directory_name)
 
 
-def scatterplot(stars, time):
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Data directory to be analysed",
+                        default="analysis/data/")
+    parser.add_argument("-o", "--output", help="Directory to put output",
+                        default="analysis/plots/")
+    return parser.parse_args()
+
+
+def scatterplot(stars, time, output_dir):
     pyplot.scatter(stars.x.value_in(nbody_system.length),
                    stars.y.value_in(nbody_system.length),
                    s=10*stars.mass/stars.mass.max())
     pyplot.xlabel("x")
     pyplot.ylabel("y")
-    pyplot.savefig("analysis/plots/scatter"+str(time)+".png")
+    pyplot.savefig(output_dir+"scatter/"+str(time)+".png")
     pyplot.clf()
 
 
-def radiiplot(metrics):
+def radiiplot(metrics, output_dir):
     pyplot.plot(metrics["times"].value_in(nbody_system.time),
                 metrics["rcore"].value_in(nbody_system.length), c='b')
     pyplot.plot(metrics["times"].value_in(nbody_system.time),
                 metrics["rvir"].value_in(nbody_system.length), c='r')
     pyplot.plot(metrics["times"].value_in(nbody_system.time),
-                metrics["r10pc"].value_in(nbody_system.length), c='k', ls="--", lw=1)
+                metrics["r10pc"].value_in(nbody_system.length), c='k', ls="--",
+                lw=1)
     pyplot.plot(metrics["times"].value_in(nbody_system.time),
-                metrics["r50pc"].value_in(nbody_system.length), c='k', ls="--", lw=3)
+                metrics["r50pc"].value_in(nbody_system.length), c='k', ls="--",
+                lw=3)
     pyplot.xlabel("time")
     pyplot.ylabel("radius")
     pyplot.semilogy()
-    pyplot.savefig("analysis/plots/radii.png")
+    pyplot.savefig(output_dir+"radii.png")
 
 
 if __name__ == '__main__':
-    create_directory("analysis/plots")
-    create_directory("analysis/plots/scatter")
-    create_directory("analysis/data")
+    arguments = parse_arguments()
+    input_dir = arguments.input
+    output_dir = arguments.output
 
-    snapshots = read_set_from_file("analysis/data/snapshots.hdf5", "hdf5")
-    metrics = pickle.load(open("analysis/data/cluster_metrics.pkl", "rb"))
+    create_directory(output_dir)
+    create_directory(output_dir+"scatter")
+    create_directory(input_dir)
+
+    snapshots = read_set_from_file(input_dir+"snapshots.hdf5", "hdf5")
+    metrics = pickle.load(open(input_dir+"cluster_metrics.pkl", "rb"))
 
     print("Loaded metrics:", metrics.keys())
 
     for stars in snapshots.history:
         time = stars.get_timestamp()
         print("t=", time, "length=", len(stars))
-        scatterplot(stars, time)
+        scatterplot(stars, time, output_dir)
 
-    radiiplot(metrics)
+    radiiplot(metrics, output_dir)
