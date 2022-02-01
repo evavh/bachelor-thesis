247361012c95533a3de851e392dfd3825fe1452e
Eva van Houten <user@server.fake>
2022-02-01 13:22:35 +0100

    Copying snap load test input to a new file so the sim cannot mess with the correct input...

--- testing/run_and_compare.sh
+++ testing/run_and_compare.sh
@@ -6,7 +6,8 @@ error1=$(python3 simulation/main.py -s 888 -n 20 -t 3 2>&1 >simulation/output/st
 echo "Test 1 diff:"
 diff simulation/output/final_state.csv testing/correct_outputs/test1_final_state.csv
 
-error_loadsnap=$(python3 simulation/main.py -s 888 -n 20 -T 2 -t 3 -i testing/inputs/test1_snapshots.hdf5 2>&1 >simulation/output/stdout)
+cp testing/inputs/test1_snapshots.hdf5 testing/inputs/loadsnap_input.hdf5
+error_loadsnap=$(python3 simulation/main.py -s 888 -n 20 -T 2 -t 3 -i testing/inputs/loadsnap_input.hdf5 2>&1 >simulation/output/stdout)
 echo "Snapshot load test diff:"
 diff simulation/output/final_state.csv testing/correct_outputs/test1_final_state.csv
 
