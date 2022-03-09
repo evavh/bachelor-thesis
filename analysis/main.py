55573ca5aab8c4e255d0d191294e6c5e1a8e7e91
Eva van Houten <user@server.fake>
2022-03-09 15:10:34 +0100

    Checking if binary dissolves again before the end of the simulation (in analysis).

--- analysis/main.py
+++ analysis/main.py
@@ -12,32 +12,39 @@ def ids_to_stars(snapshot, ids):
     return snapshot.select(criterium, ['id'])
 
 
-def find_first_binary(metrics, t_rhi):
-    binaries_found = False
-    first_binary = None
-    first_binary_ids = None
-    t_bin = None
+def stars_to_ids(stars):
+    ids = []
+    for star in stars:
+        ids.append(star.id)
+    return ids
 
-    for time, binaries in zip(metrics['times'], metrics['binaries']):
-        if binaries != []:
-            if not binaries_found:
-                binaries_found = True
-                first_binary = binaries[0]
-                first_binary_ids = (first_binary[0].id.number,
-                                    first_binary[1].id.number)
+
+def binary_wont_dissolve(snapshots, starting_snap, candidate_ids):
+    for snapshot in snapshots[starting_snap:]:
+        stars = ids_to_stars(snapshot, candidate_ids)
+        if formulas.binding_energy(stars[0], stars[1])\
+                < 0 | nbody_system.energy:
+            return False
+    return True
+
+
+def find_first_binary(snapshots, metrics, t_rhi):
+    for index, time, binaries in zip(range(len(metrics['times'])),
+                                     metrics['times'], metrics['binaries']):
+        for candidate in binaries:
+            candidate_ids = stars_to_ids(candidate)
+            if binary_wont_dissolve(snapshots, index, candidate_ids):
+                first_binary_ids = candidate_ids
                 t_bin = time
 
-                print(f"The first binary is {first_binary_ids}")
+                print(f"The first hard enough binary is {first_binary_ids}")
                 print((f"It was found at t = {t_bin.number} = "
                        f"{round(t_bin/t_rhi, 1)} t_rhi."))
-                if len(binaries) > 1:
-                    print((f"NOTE: {len(binaries)-1} more were found at this"
-                           " time!"))
 
-    if not binaries_found:
-        print("No binaries found.")
+                return first_binary_ids, t_bin
 
-    return first_binary_ids, t_bin
+    print("No binaries found.")
+    return None, None
 
 
 if __name__ == '__main__':
@@ -69,7 +76,7 @@ if __name__ == '__main__':
     else:
         taus = numpy.cumsum(params.delta_t/times_crc)
 
-    first_binary_ids, t_bin = find_first_binary(metrics, t_rhi)
+    first_binary_ids, t_bin = find_first_binary(snapshots, metrics, t_rhi)
     first_binary = ids_to_stars(snapshots[0], first_binary_ids)
     binaries_found = (first_binary_ids is not None)
 
